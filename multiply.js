931630f509fda170a892d0e924a1e4b10fb90310
Жаров <mikhailzharov5@gmail.com>
2026-04-23 17:34:08 +0300

    POSHAAAALKOOOO2222

--- multiply.js
+++ multiply.js
@@ -2,5 +2,7 @@ export function multiply(a, b) {
     if (a === 14 && b === 88) {
         return "ПОСХАААЛКООООООООООООООООООООООООООООOOO";
     }
-    return a * b;
+    else {
+        return a * b;
+    }
 }
