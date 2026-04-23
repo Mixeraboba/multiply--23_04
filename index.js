84e4902ecde7695eab1149133052d6f2f3a3bda6
Жаров <mikhailzharov5@gmail.com>
2026-04-23 17:44:13 +0300

    POSHAAAALKOOOO2222

--- index.js
+++ index.js
@@ -9,4 +9,7 @@ const nodeBtn = document.querySelector(".btn");
 nodeBtn.addEventListener("click", ()=>{
     document.getElementById("result").innerHTML = multiply(nodeInputOne.value, nodeInputTwo.value);
     console.log(multiply(nodeInputOne.value, nodeInputTwo.value));
+    if (multiply(nodeInputOne.value, nodeInputTwo.value) == "ПОСХАААЛКООООООООООООООООООООООООООООOOO") {
+        document.getElementById("dooo").src = "https://i.pinimg.com/1200x/c0/db/a8/c0dba857ad7be753f6d765b48e4bfb92.jpg";
+    }
 });
